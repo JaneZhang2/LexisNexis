f364d256aa52b3ecf446545179a9b73e89eb98ca
Jane Zhang <jane.zhang.1@lexisnexis.com>
2015-09-09 17:59:17 +0800

    "grunt-contrib-clean": "^0.6.0", "grunt-contrib-copy": "^0.8.1"

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,16 +17,30 @@
       // Project settings
       config: config,
 
+      // Empties folders to start fresh
+      clean: {
+        lnc: '<%= config.lnc.dist %>'
+      },
+
+      // Copies remaining files to places other tasks can use
+      copy: {
+        lnc: {
+          files: {
+            '<%= config.lnc.dist %>/index.html': '<%= config.lnc.app %>/index.html'
+          }
+        }
+      },
+
       // Automatically inject Bower components into the app
       wiredep: {
         lnc: {
-          src: ['<%= config.lnc.app %>/index.html']
+          src: ['<%= config.lnc.dist %>/index.html']
         }
       }
 
     });
 
-    grunt.registerTask('default', ['wiredep']);
+    grunt.registerTask('default', ['clean', 'copy', 'wiredep']);
   };
 
 })();
