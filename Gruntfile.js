4a0e456fa1491986dd59b77c36693e76d6d4cccd
Jane Zhang <jane.zhang.1@lexisnexis.com>
2015-09-09 17:37:52 +0800

    config task：wiredep

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,30 @@
 
   module.exports = function (grunt) {
     require('jit-grunt')(grunt);
+
+    var config = {
+      lnc: {
+        app: 'lnc/app',
+        dist: 'lnc/dist'
+      }
+    };
+
+    // Define the configuration for all the tasks
+    grunt.initConfig({
+
+      // Project settings
+      config: config,
+
+      // Automatically inject Bower components into the app
+      wiredep: {
+        lnc: {
+          src: ['<%= config.lnc.app %>/index.html']
+        }
+      }
+
+    });
+
+    grunt.registerTask('default', ['wiredep']);
   };
 
 })();
