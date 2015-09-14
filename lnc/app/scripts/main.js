4210737c68c8d85c411e14991c7f1e3b982a5991
janezhang2 <janezhang2@yeah.net>
2015-09-14 16:08:46 +0800

    "angular": "1.2.16", "json2": "*", "json3": "~3.3.2",

--- lnc/app/scripts/main.js
+++ lnc/app/scripts/main.js
@@ -1,17 +1,31 @@
 require.config({
   paths: {
-    'angular': '../../../bower_components/angular/angular',
+    angular: '../../../bower_components/angular/angular',
     'angular-route': '../../../bower_components/angular-route/angular-route',
-    'jquery': '../../../bower_components/jquery/dist/jquery',
+    jquery: '../../../bower_components/jquery/dist/jquery',
     'angular-ui-router': '../../../bower_components/angular-ui-router/release/angular-ui-router',
-    'oclazyload': '../../../bower_components/oclazyload/dist/ocLazyLoad.require'
+    oclazyload: '../../../bower_components/oclazyload/dist/ocLazyLoad',
+    json3: '../../../bower_components/json3/lib/json3',
+    json2: '../../../bower_components/json2/json2'
   },
   shim: {
-    'angular': ['jquery'],
-    'angular-ui-router': ['angular'],
-    'oclazyload': ['angular'],
-    'lnc': ['angular-ui-router', 'oclazyload']
-  }
+    angular: [
+      'jquery'
+    ],
+    'angular-ui-router': [
+      'angular'
+    ],
+    oclazyload: [
+      'angular'
+    ],
+    lnc: [
+      'angular-ui-router',
+      'oclazyload'
+    ]
+  },
+  packages: [
+
+  ]
 });
 
 require(['lnc'], function () {
