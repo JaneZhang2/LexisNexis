ce6aee68a2625c2506f60bcda66d0f3706a1794a
janezhang2 <janezhang2@yeah.net>
2015-09-22 15:55:37 +0800

    content_types.js

--- app/scripts/controllers/SearchQuickController.js
+++ app/scripts/controllers/SearchQuickController.js
@@ -1,21 +1,4 @@
 angular.module('lnc')
-  .controller('SearchQuickController', function (content_types, $scope) {
-    // status:
-    // 0:无；
-    // 1：selected；
-
-    // $scope.content_types = {
-    //   'LEGISLATION': true,
-    //   'NATION_STANDARD': false,
-    //   'LEGAL_NEWS': false,
-    //   'CASES': false,
-    //   'NEWSLETTERS': false,
-    //   'PRACTICAL_MATERICAL': false,
-    //   'EXPERT_ANALYSIS': false,
-    //   'LEXIS_NEWSLETTERS': false,
-    //   'FOREIGN_LAWS': false,
-    //   'SEARCH_ALL': false
-    // };
-
+  .controller('SearchQuickController', function ($scope, content_types) {
     $scope.content_types = content_types;
   });
